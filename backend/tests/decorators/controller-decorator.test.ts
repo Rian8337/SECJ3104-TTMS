5c6013954aeef1e3f8c1def20cbba0e5c1b60412
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-26 08:23:07 +0800

    Add tests for `@Controller` decorator

---
+++ backend/tests/decorators/controller-decorator.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest";
+import { Controller } from "../../src/decorators/controller";
+import { createMockClassDecoratorTestTarget } from "../mocks/mockDecoratorTargetFactory";
+
+describe("@Controller", () => {
+    it("Adds controller metadata", () => {
+        const t = createMockClassDecoratorTestTarget(Controller, "/test");
+
+        const basePath = Reflect.getMetadata("basePath", t) as string;
+
+        expect(basePath).toBeDefined();
+        expect(basePath).toBe("/test");
+
+        const controllers = Reflect.getMetadata(
+            "controllers",
+            globalThis
+        ) as unknown[];
+
+        expect(controllers).toBeDefined();
+        expect(Array.isArray(controllers)).toBe(true);
+        expect(controllers).toContain(t);
+    });
+});

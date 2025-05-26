ec42bde98c899a586ab5a2b7e1bd002fa8c2f33b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-26 21:23:16 +0800

    Add test for `@Controller` decorator with default path

--- backend/tests/decorators/controller-decorator.test.ts
+++ backend/tests/decorators/controller-decorator.test.ts
@@ -3,7 +3,25 @@ import { Controller } from "../../src/decorators/controller";
 import { createMockClassDecoratorTestTarget } from "../mocks";
 
 describe("@Controller", () => {
-    it("Adds controller metadata", () => {
+    it("Adds controller metadata with the default path", () => {
+        const t = createMockClassDecoratorTestTarget(Controller);
+
+        const basePath = Reflect.getMetadata("basePath", t) as string;
+
+        expect(basePath).toBeDefined();
+        expect(basePath).toBe("");
+
+        const controllers = Reflect.getMetadata(
+            "controllers",
+            globalThis
+        ) as unknown[];
+
+        expect(controllers).toBeDefined();
+        expect(Array.isArray(controllers)).toBe(true);
+        expect(controllers).toContain(t);
+    });
+
+    it("Adds controller metadata with a custom path", () => {
         const t = createMockClassDecoratorTestTarget(Controller, "/test");
 
         const basePath = Reflect.getMetadata("basePath", t) as string;

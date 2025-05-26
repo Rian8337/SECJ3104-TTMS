f4871abb16436e2cb713864a05113af50deae947
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-26 21:01:38 +0800

    Add test for `@Roles` decorator that verifies the middleware checks for user roles

--- backend/tests/decorators/roles-decorator.test.ts
+++ backend/tests/decorators/roles-decorator.test.ts
@@ -1,20 +1,52 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { Roles } from "../../src/decorators/roles";
 import { UserRole } from "../../src/types";
-import { createMockMethodDecoratorTestTarget } from "../mocks";
+import {
+    createMockMethodDecoratorTestTarget,
+    createMockRequest,
+    createMockResponse,
+    mockAuthService,
+} from "../mocks";
+import { RequestHandler } from "express";
 
 describe("@Roles", () => {
     it("Adds middleware metadata", () => {
         const t = createMockMethodDecoratorTestTarget(Roles, UserRole.student);
 
-        const middleware = Reflect.getMetadata(
+        const middlewares = Reflect.getMetadata(
             "route:middlewares",
             t.prototype,
             t.methodName
-        ) as string[];
+        ) as RequestHandler[];
 
-        expect(middleware).toBeDefined();
-        expect(Array.isArray(middleware)).toBe(true);
-        expect(middleware.length).toBe(1);
+        expect(middlewares).toBeDefined();
+        expect(Array.isArray(middlewares)).toBe(true);
+        expect(middlewares.length).toBe(1);
+    });
+
+    it("Adds middleware that checks user role", async () => {
+        mockAuthService.verifySession.mockReturnValueOnce((req, res, next) => {
+            next();
+        });
+
+        const t = createMockMethodDecoratorTestTarget(Roles, UserRole.student);
+
+        const middlewares = Reflect.getMetadata(
+            "route:middlewares",
+            t.prototype,
+            t.methodName
+        ) as RequestHandler[];
+
+        expect(middlewares).toBeDefined();
+        expect(middlewares.length).toBe(1);
+
+        const middleware = middlewares[0];
+        expect(middleware).toBeInstanceOf(Function);
+
+        await middleware(createMockRequest(), createMockResponse(), vi.fn());
+
+        expect(mockAuthService.verifySession).toHaveBeenCalledWith(
+            UserRole.student
+        );
     });
 });

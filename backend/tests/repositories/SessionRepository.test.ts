b356b6c9f9a23cc8d7f5bfccabc1595753e46ab3
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-27 11:05:53 +0800

    Add tests for `SessionRepository`

---
+++ backend/tests/repositories/SessionRepository.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { SessionRepository } from "../../src/repositories";
+import { mockDb } from "../mocks";
+import { sessions } from "../../src/database/schema";
+
+describe("SessionRepository (unit)", () => {
+    it("Should call database", async () => {
+        mockDb.select.mockReturnValueOnce({
+            from: mockDb.from.mockResolvedValueOnce([]),
+        });
+
+        const repository = new SessionRepository(mockDb);
+
+        await repository.getSessions();
+
+        expect(mockDb.select).toHaveBeenCalled();
+        expect(mockDb.from).toHaveBeenCalledWith(sessions);
+    });
+});

166ca14a1970fb3d359a8c5a5a811edd9e94e79e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-06-02 21:07:37 +0800

    Add test for `VenueRepository.getVenueClashes`

--- backend/tests/repositories/VenueRepository.test.ts
+++ backend/tests/repositories/VenueRepository.test.ts
@@ -1,13 +1,18 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { venues } from "../../src/database/schema";
 import { VenueRepository } from "../../src/repositories";
 import { createMockDb } from "../mocks";
 
 describe("VenueRepository (unit)", () => {
-    it("Should call database", async () => {
-        const mockDb = createMockDb();
-        const repository = new VenueRepository(mockDb);
+    let repository: VenueRepository;
+    let mockDb: ReturnType<typeof createMockDb>;
 
+    beforeEach(() => {
+        mockDb = createMockDb();
+        repository = new VenueRepository(mockDb);
+    });
+
+    it("[getByCode] Should call database", async () => {
         mockDb.limit.mockResolvedValueOnce([]);
 
         await repository.getByCode("");
@@ -22,5 +27,26 @@ describe("VenueRepository (unit)", () => {
 
         expect(mockDb.limit).toHaveBeenCalledWith(1);
         expect(mockDb.limit).toHaveBeenCalledAfter(mockDb.where);
+    });
+
+    it("[getVenueClashes] Should call database", async () => {
+        await repository.getVenueClashes("2024/2025", 2);
+
+        expect(mockDb.selectDistinct).toHaveBeenCalledOnce();
+
+        expect(mockDb.from).toHaveBeenCalledOnce();
+        expect(mockDb.from).toHaveBeenCalledAfter(mockDb.selectDistinct);
+
+        expect(mockDb.innerJoin).toHaveBeenCalledTimes(4);
+        expect(mockDb.leftJoin).toHaveBeenCalledOnce();
+
+        expect(mockDb.where).toHaveBeenCalledOnce();
+        expect(mockDb.where).toHaveBeenCalledAfter(mockDb.innerJoin);
+
+        expect(mockDb.orderBy).toHaveBeenCalledOnce();
+        expect(mockDb.orderBy).toHaveBeenCalledAfter(mockDb.where);
+
+        expect(mockDb.execute).toHaveBeenCalledOnce();
+        expect(mockDb.execute).toHaveBeenCalledAfter(mockDb.orderBy);
     });
 });

926f7eed3860a7e53f8c5b80de47b59a84edbb36
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-06-05 19:43:01 +0800

    Add integration tests for `StudentService`

--- backend/tests/services/StudentService.test.ts
+++ backend/tests/services/StudentService.test.ts
@@ -1,7 +1,22 @@
-import { IStudent } from "@/database/schema";
-import { StudentService, FailedOperationResult } from "@/services";
-import { IRawTimetable } from "@/types";
+import { dependencyTokens } from "@/dependencies/tokens";
+import {
+    FailedOperationResult,
+    StudentService,
+    SuccessfulOperationResult,
+} from "@/services";
+import {
+    CourseSectionScheduleDay,
+    CourseSectionScheduleTime,
+    IStudentSearchEntry,
+    ITimetable,
+} from "@/types";
 import { mockStudentRepository } from "../mocks";
+import { createTestContainer } from "../setup/container";
+import {
+    cleanupSecondaryTables,
+    seededPrimaryData,
+    seeders,
+} from "../setup/db";
 
 describe("StudentService (unit)", () => {
     let service: StudentService;
@@ -19,58 +34,6 @@ describe("StudentService (unit)", () => {
     });
 
     describe("getTimetable", () => {
-        it("Should get timetable for student", async () => {
-            mockStudentRepository.getByMatricNo.mockResolvedValueOnce({
-                matricNo: "A12345678",
-                name: "John Doe",
-                courseCode: "SECJH",
-                facultyCode: "FSKSM",
-                kpNo: "123456789012",
-            } satisfies IStudent);
-
-            mockStudentRepository.getTimetable.mockResolvedValueOnce([
-                {
-                    courseCode: "CS101",
-                    courseName: "Introduction to Computer Science",
-                    section: "1",
-                    lecturerName: "John Doe",
-                    lecturerNo: 123456,
-                    scheduleDay: 1,
-                    scheduleTime: 2,
-                    venueShortName: "Room 101",
-                },
-                {
-                    courseCode: "CS102",
-                    courseName: "Data Structures",
-                    section: "1",
-                    lecturerName: "John Doe",
-                    lecturerNo: 123456,
-                    scheduleDay: 1,
-                    scheduleTime: 3,
-                    venueShortName: "Room 102",
-                },
-            ] satisfies IRawTimetable[]);
-
-            const result = await service.getTimetable(
-                "A12345678",
-                "2023/2024",
-                1
-            );
-
-            expect(result.isSuccessful()).toBe(true);
-            expect(result.failed()).toBe(false);
-
-            expect(mockStudentRepository.getByMatricNo).toHaveBeenCalledWith(
-                "A12345678"
-            );
-
-            expect(mockStudentRepository.getTimetable).toHaveBeenCalledWith(
-                "A12345678",
-                "2023/2024",
-                1
-            );
-        });
-
         it("Should return error for non-existent student timetable", async () => {
             mockStudentRepository.getByMatricNo.mockResolvedValueOnce(null);
 
@@ -136,13 +99,7 @@ describe("StudentService (unit)", () => {
         });
 
         it("Should search by matric number", async () => {
-            const result = await service.search(
-                "2024/2025",
-                1,
-                "A12FS5678",
-                10,
-                0
-            );
+            const result = await service.search("2024/2025", 1, "A12FS5678");
 
             expect(result.isSuccessful()).toBe(true);
             expect(result.failed()).toBe(false);
@@ -176,22 +133,197 @@ describe("StudentService (unit)", () => {
                 mockStudentRepository.searchByMatricNo
             ).not.toHaveBeenCalled();
         });
+    });
+});
 
-        it("Should search by matric number with default limit and offset", async () => {
-            const result = await service.search("2024/2025", 1, "A12FS5678");
+describe("StudentService (integration)", () => {
+    const container = createTestContainer();
+    const service = container.resolve(dependencyTokens.studentService);
+
+    describe("getTimetable", () => {
+        const session = seededPrimaryData.sessions[0];
+        const course = seededPrimaryData.courses[0];
+        const student = seededPrimaryData.students[0];
+        const lecturer = seededPrimaryData.lecturers[0];
+        const venue = seededPrimaryData.venues[0];
+
+        beforeAll(async () => {
+            const section = await seeders.courseSection.seedOne({
+                courseCode: course.code,
+                section: "1",
+                session: session.session,
+                semester: session.semester,
+                lecturerNo: lecturer.workerNo,
+            });
+
+            await seeders.courseSectionSchedule.seedMany(
+                {
+                    courseCode: section.courseCode,
+                    section: section.section,
+                    day: CourseSectionScheduleDay.monday,
+                    session: section.session,
+                    semester: section.semester,
+                    time: CourseSectionScheduleTime.time2,
+                    venueCode: venue.code,
+                },
+                {
+                    courseCode: section.courseCode,
+                    section: section.section,
+                    day: CourseSectionScheduleDay.monday,
+                    session: section.session,
+                    semester: section.semester,
+                    time: CourseSectionScheduleTime.time3,
+                    venueCode: venue.code,
+                }
+            );
+
+            await seeders.studentRegisteredCourse.seedOne({
+                matricNo: student.matricNo,
+                courseCode: section.courseCode,
+                section: section.section,
+                session: section.session,
+                semester: section.semester,
+            });
+        });
+
+        afterAll(cleanupSecondaryTables);
+
+        it("Should get timetable for student", async () => {
+            const result = (await service.getTimetable(
+                student.matricNo,
+                session.session,
+                session.semester
+            )) as SuccessfulOperationResult<ITimetable[]>;
 
             expect(result.isSuccessful()).toBe(true);
             expect(result.failed()).toBe(false);
 
-            expect(mockStudentRepository.searchByMatricNo).toHaveBeenCalledWith(
-                "2024/2025",
-                1,
-                "A12FS5678",
-                10,
-                0
-            );
+            expect(result.data).toBeInstanceOf(Array);
+            expect(result.data).toHaveLength(2);
 
-            expect(mockStudentRepository.searchByName).not.toHaveBeenCalled();
+            expect(result.data[0]).toStrictEqual({
+                courseSection: {
+                    course: {
+                        code: course.code,
+                        name: course.name,
+                    },
+                    lecturer,
+                    section: "1",
+                },
+                day: CourseSectionScheduleDay.monday,
+                time: CourseSectionScheduleTime.time2,
+                venue: { shortName: venue.shortName },
+            } satisfies ITimetable);
+
+            expect(result.data[1]).toStrictEqual({
+                courseSection: {
+                    course: {
+                        code: course.code,
+                        name: course.name,
+                    },
+                    lecturer,
+                    section: "1",
+                },
+                day: CourseSectionScheduleDay.monday,
+                time: CourseSectionScheduleTime.time3,
+                venue: { shortName: venue.shortName },
+            } satisfies ITimetable);
+        });
+    });
+
+    describe("search", () => {
+        const session = seededPrimaryData.sessions[0];
+        const student = seededPrimaryData.students[0];
+
+        beforeAll(async () => {
+            const lecturer = seededPrimaryData.lecturers[0];
+            const course = seededPrimaryData.courses[0];
+
+            const section = await seeders.courseSection.seedOne({
+                courseCode: course.code,
+                section: "1",
+                session: session.session,
+                semester: session.semester,
+                lecturerNo: lecturer.workerNo,
+            });
+
+            await seeders.studentRegisteredCourse.seedOne({
+                matricNo: student.matricNo,
+                courseCode: section.courseCode,
+                section: section.section,
+                session: section.session,
+                semester: section.semester,
+            });
+        });
+
+        afterAll(cleanupSecondaryTables);
+
+        it("Should return empty result if no match is found", async () => {
+            const result = (await service.search(
+                session.session,
+                session.semester,
+                "NonExistentStudent"
+            )) as SuccessfulOperationResult<IStudentSearchEntry[]>;
+
+            expect(result.isSuccessful()).toBe(true);
+            expect(result.failed()).toBe(false);
+
+            expect(result.data).toBeInstanceOf(Array);
+            expect(result.data).toHaveLength(0);
+        });
+
+        it("Should return empty if student is not in offset range", async () => {
+            const result = (await service.search(
+                session.session,
+                session.semester,
+                student.name,
+                1,
+                10
+            )) as SuccessfulOperationResult<IStudentSearchEntry[]>;
+
+            expect(result.isSuccessful()).toBe(true);
+            expect(result.failed()).toBe(false);
+
+            expect(result.data).toBeInstanceOf(Array);
+            expect(result.data).toHaveLength(0);
+        });
+
+        it("Should search by matric number", async () => {
+            const result = (await service.search(
+                session.session,
+                session.semester,
+                student.matricNo
+            )) as SuccessfulOperationResult<IStudentSearchEntry[]>;
+
+            expect(result.isSuccessful()).toBe(true);
+            expect(result.failed()).toBe(false);
+
+            expect(result.data).toBeInstanceOf(Array);
+            expect(result.data).toHaveLength(1);
+            expect(result.data[0]).toStrictEqual({
+                courseCode: student.courseCode,
+                matricNo: student.matricNo,
+                name: student.name,
+            } satisfies IStudentSearchEntry);
+        });
+
+        it("Should search by name", async () => {
+            const result = (await service.search(
+                session.session,
+                session.semester,
+                student.name.split(" ")[0]
+            )) as SuccessfulOperationResult<IStudentSearchEntry[]>;
+
+            expect(result.isSuccessful()).toBe(true);
+            expect(result.failed()).toBe(false);
+
+            expect(result.data).toBeInstanceOf(Array);
+            expect(result.data).toHaveLength(1);
+            expect(result.data[0]).toStrictEqual({
+                courseCode: student.courseCode,
+                matricNo: student.matricNo,
+                name: student.name,
+            } satisfies IStudentSearchEntry);
         });
     });
 });
